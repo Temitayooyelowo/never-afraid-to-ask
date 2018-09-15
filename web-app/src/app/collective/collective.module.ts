a241fa3a0d73fe2e8d71490694b924631d7b0540
Temitayo Oyelowo <temitayooyelowo@gmail.com>
2018-09-15 07:05:44 -0400

    Start modifying the signin component

--- web-app/src/app/collective/collective.module.ts
+++ web-app/src/app/collective/collective.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { DropdownDirective } from './dropdown.directive';
 
 @NgModule({
   declarations: [
-    DropdownDirective
   ],
   exports: [
-    CommonModule,
-    DropdownDirective
+    CommonModule
   ]
 })
 
