0fe2b411804626a90e627ef8b631c1fa54cd7f63
wenyuedeng <deviny-deng@outlook.com>
2018-09-15 23:22:19 -0400

    updated UI Design

--- web-app/src/app/app-routing.module.ts
+++ web-app/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './core/home/home.component';
 import { UserComponent } from './user/user.component';
+import { AboutUsComponent } from './aboutus/aboutus.component';
 
 const appRoutes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'user', component: UserComponent},
+  {path: 'aboutus', component: AboutUsComponent},
   {path: '', pathMatch: 'full', redirectTo: 'home'}
 ];
 
--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { CoreModule } from './core/core.module';
 import { AuthModule } from './auth/auth.module';
 import { WildCardRoutingModule } from './core/page-not-found/wildcard-routing.module';
 import { UserComponent } from './user/user.component';
+import { AboutUsComponent } from './aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
