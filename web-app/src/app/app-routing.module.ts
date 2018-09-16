6bfe80bf23561f470af8a71dfe35d1815fe7021a
wenyuedeng <deviny-deng@outlook.com>
2018-09-16 05:06:58 -0400

    updated

--- web-app/src/app/app-routing.module.ts
+++ web-app/src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { HomeComponent } from './core/home/home.component';
 import { UserComponent } from './user/user.component';
 import { ChatAppComponent } from './chat-app/chat-app.component';
 import { AboutUsComponent } from './aboutus/aboutus.component';
-
+import { HowToComponent } from './howto/howto.component';
 const appRoutes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'user', component: UserComponent},
   {path: 'chat-app', component: ChatAppComponent},
   {path: 'aboutus', component: AboutUsComponent},
+  {path: 'howto', component: HowToComponent},
   {path: '', pathMatch: 'full', redirectTo: 'home'}
 ];
 
